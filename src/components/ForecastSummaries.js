5826a75834678c5eed7e314e1afd066ce9084c33
Ben-H123 <ben_h321@protonmail.com>
2024-02-20 21:33:44 +0000

    more progress with testing and CSS

---
+++ src/components/ForecastSummaries.js
@@ -0,0 +1,20 @@
+/* eslint-disable react/function-component-definition */
+import React from 'react';
+import ForecastSummary from './ForecastSummary';
+import '../styles/ForecastSummaries.css';
+
+const ForecastSummaries = ({forecasts}) => (
+    <div className="forecast-summaries">
+      {forecasts.map(forecast => (
+        <ForecastSummary
+          key={forecast.date}
+          date={forecast.date}
+          description={forecast.description}
+          icon={forecast.icon}
+          temperature={forecast.temperature}
+        />
+      ))}
+    </div>
+  );
+
+export default ForecastSummaries;
