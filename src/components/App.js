0059ec20d9f2a5bba9aff9f9959d7b86368ae608
Ben-H123 <ben_h321@protonmail.com>
2024-02-26 21:34:08 +0000

    added search form

--- src/components/App.js
+++ src/components/App.js
@@ -5,12 +5,17 @@ import LocationDetails from './LocationDetails';
 import ForecastSummaries from './ForecastSummaries';
 import ForecastDetails from './ForecastDetails';
 import getForecast from '../requests/getForecast';
+import SearchForm from '../SearchForm';
 
 function App() {
   const [forecasts, setForecasts] = useState([]);
   const [location, setLocation] = useState({ city: "", country: ""});
   const [selectedDate, setSelectedDate] = useState(0);
+  const [searchText, setSearchText] = useState("");
   const selectedForecast = forecasts.find((forecast) => forecast.date === selectedDate);
+  const handleCitySearch = () => {
+    getForecast(setSelectedDate, setForecasts, setLocation);
+  }
 
   const handleForecastSelect = (date) => {
   setSelectedDate(date);
@@ -24,6 +29,7 @@ function App() {
   return (
     <div className="weather-app">
       <LocationDetails city={location.city} country={location.country} />
+      <SearchForm searchText={searchText} setSearchText={setSearchText}/>
       <ForecastSummaries forecasts={forecasts} onForecastSelect={handleForecastSelect} />
       {selectedForecast && <ForecastDetails forecast={selectedForecast} />}
     </div>
